e1e32cac567b00cae66ba49cd78e0f1c9ed9bf05
Rick-van-Leeuwen <rickvleeuwen10@gmail.com>
2025-11-19 14:12:05 +0100

    Some experiments + rudimentary checkpoint system

---
+++ Stylish Thief/Assets/RickTest/Scripts/RickDeathManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RickDeathManager : MonoBehaviour
+{
+    public GameObject currentCheckpoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

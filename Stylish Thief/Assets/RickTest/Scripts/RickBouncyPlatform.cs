da440a2e51e0a8e299c943276fa57f9b8ddd8896
Rick-van-Leeuwen <rickvleeuwen10@gmail.com>
2025-11-26 17:45:55 +0100

    Rope Pull is finished plus some camera changes

--- Stylish Thief/Assets/RickTest/Scripts/RickBouncyPlatform.cs
+++ Stylish Thief/Assets/RickTest/Scripts/RickBouncyPlatform.cs
@@ -16,33 +16,43 @@ public class RickBouncyPlatform : MonoBehaviour
             player = other.gameObject.GetComponentInParent<PlayerStateDriver>();
             player.ctx.hasGrabbed = false;
             directionalForce = transform.up * launchForce;
-            if (oneToOne)
+            /*if (oneToOne)
             {
                 
                 if (player.ctx.rb.velocity.y > -minimumHeight * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10))
                 {
                     player.ctx.rb.velocity = transform.up * minimumHeight;
-                    player.Machine.ChangeState(player.Root.Leaf(), player.Root.airborne);
                 }
                 else
                 {
                     player.ctx.rb.velocity = new Vector3(player.ctx.rb.velocity.x, -player.ctx.rb.velocity.y * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10), player.ctx.rb.velocity.z);
                     //player.ctx.rb.velocity = transform.up * (-player.ctx.rb.velocity.y * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10));
-                    player.Machine.ChangeState(player.Root.Leaf(), player.Root.airborne);
+                }
+            }*/
+            if (oneToOne)
+            {
+                //Check for launch magnitude here as the check for minimum launch
+                if (player.ctx.rb.velocity.y > -minimumHeight)
+                {
+                    player.SetVelocity(transform.up * minimumHeight);
+                }
+                else
+                {
+                    //player.ctx.rb.velocity = new Vector3(player.ctx.rb.velocity.x, -player.ctx.rb.velocity.y * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10), player.ctx.rb.velocity.z);
+                    player.SetVelocity(transform.up * (-player.ctx.rb.velocity.y * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10)));
                 }
             }
             else if (fullSpeedReplacement)
             {
-                player.ctx.rb.velocity = directionalForce;
-                player.Machine.ChangeState(player.Root.Leaf(), player.Root.airborne);
+                player.SetVelocity(directionalForce);
             }
             else
             {
-                player.ctx.rb.velocity = player.ctx.rb.velocity - new Vector3(0, player.ctx.rb.velocity.y, 0) + directionalForce;
-                player.Machine.ChangeState(player.Root.Leaf(), player.Root.airborne);
+                player.SetVelocity(player.ctx.rb.velocity - new Vector3(0, player.ctx.rb.velocity.y, 0) + directionalForce);
             }
         }
     }
+
 
     /* RICK'S LITTLE CHEAT SHEET
     private void OnTriggerEnter(Collider other)
@@ -66,7 +76,7 @@ public class RickBouncyPlatform : MonoBehaviour
             player.transform.position = player.transform.position + new Vector3(0, 10, 0);
 
             //Changing the player's speed
-            player.ctx.rb.velocity = Vector3.up * 50;
+            player.SetVelocity(transform.up * minimumHeight);
 
             //Changing the player's state (probably don't need this but handy to have anyway)
             player.Machine.ChangeState(player.Root.Leaf(), player.Root.airborne);

f2e2ed2f15869cf871c2dd4d5ad505b974b68747
nika <>
2025-12-18 15:42:58 +0100

    New jump mechanics!!! have fun rick!!!

--- Stylish Thief/Assets/RickTest/Scripts/RickBouncyPlatform.cs
+++ Stylish Thief/Assets/RickTest/Scripts/RickBouncyPlatform.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RickBouncyPlatform : MonoBehaviour
 {
     public float launchForce;
+    public float oneToOneConstant = 1.5f;
     public float minimumHeight;
     public bool oneToOne;
     public bool fullSpeedReplacement;
@@ -39,7 +40,7 @@ public class RickBouncyPlatform : MonoBehaviour
                 else
                 {
                     //player.ctx.rb.velocity = new Vector3(player.ctx.rb.velocity.x, -player.ctx.rb.velocity.y * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10), player.ctx.rb.velocity.z);
-                    player.SetVelocity(transform.up * (-player.ctx.rb.velocity.y * (1 + player.ctx.baseJumpData.downwardMovementMultiplier / 10)));
+                    player.SetVelocity(transform.up * (-player.ctx.rb.velocity.y * oneToOneConstant));
                 }
             }
             else if (fullSpeedReplacement)

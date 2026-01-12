004171b073a74e6e34a1c17231c6bf7883283558
Rick-van-Leeuwen <124429419+Rick-van-Leeuwen@users.noreply.github.com>
2026-01-12 15:50:24 +0100

    Start script experiment

---
+++ Stylish Thief/Assets/RickTest/Scripts/StartTimer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StartTimer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//Find player
+//Freeze them
+//Start counting down
+//Enable player after countdown
+//Maybe enable the whole crime spree manager

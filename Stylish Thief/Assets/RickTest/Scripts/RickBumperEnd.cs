018435e2c610c9ca31b6d2570f0dc7415b1de175
Rick-van-Leeuwen <124429419+Rick-van-Leeuwen@users.noreply.github.com>
2026-03-10 12:07:31 +0100

    Bumper finished. Will probably make alternate versions in the future

---
+++ Stylish Thief/Assets/RickTest/Scripts/RickBumperEnd.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class RickBumperEnd : MonoBehaviour
+{
+    public bool justGravity;
+    PlayerStateDriver player;
+    public GameObject playerPrefab;
+    PlayerStateDriver prefabPlayer;
+    public float endSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        prefabPlayer = playerPrefab.GetComponent<PlayerStateDriver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            //player.EnableControls();
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            
+            player = other.gameObject.GetComponentInParent<PlayerStateDriver>();
+            if (justGravity)
+            {
+                player.gameObject.GetComponent<ActorPhysics>().gravity = -Vector3.up;
+            }
+            else
+            {
+                player.EnableControls();
+                player.Machine.ChangeState(player.Root.Leaf(), player.Root.airborne.falling);
+                player.gameObject.GetComponent<ActorPhysics>().gravity = -Vector3.up;
+                player.ctx.cmd.deceleration = prefabPlayer.ctx.airMoveData.deceleration;
+                player.ctx.cmd.maxSpeedDeceleration = prefabPlayer.ctx.airMoveData.maxSpeedDeceleration;
+                //player.SetVelocity(transform.forward * endSpeed);
+                player.ctx.hasGrabbed = false;
+                player.EnableControls();
+            }
+        }
+    }
+}

0bbd593a42437200a86b17b090aa2c2c74d6a300
Rick-van-Leeuwen <rickvleeuwen10@gmail.com>
2026-03-13 17:01:34 +0100

    All basic moves tuned! Also birds made slightly smaller for now

--- Stylish Thief/Assets/RickTest/Scripts/RickBumperEnd.cs
+++ Stylish Thief/Assets/RickTest/Scripts/RickBumperEnd.cs
@@ -41,7 +41,6 @@ public class RickBumperEnd : MonoBehaviour
                 player.ctx.cmd.maxSpeedDeceleration = prefabPlayer.ctx.airMoveData.maxSpeedDeceleration;
                 //player.SetVelocity(transform.forward * endSpeed);
                 player.ctx.hasGrabbed = false;
-                player.EnableControls();
             }
         }
     }

775c1aac9053c67ad8d095a9c5a7a4214e3cbf34
Rick-van-Leeuwen <124429419+Rick-van-Leeuwen@users.noreply.github.com>
2026-04-09 15:59:01 +0200

    Some animation and movement bug fixes. More to come

--- Stylish Thief/Assets/RickTest/Scripts/BigBlasterAttack.cs
+++ Stylish Thief/Assets/RickTest/Scripts/BigBlasterAttack.cs
@@ -3,10 +3,10 @@ using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class BigBlasterAttack : EnemyAttack
+public class BigBlasterAttack : MonoBehaviour//EnemyAttack
 {
     [Header("Big Blast")]
-    public BigBlasterJumpData bigBlastData;
+    //public BigBlasterJumpData bigBlastData;
     public JumperContext jumperContext = new();
     //public float dropkickSpeed;
     //public float recoveryTime = 4;
@@ -32,41 +32,41 @@ public class BigBlasterAttack : EnemyAttack
         agent = GetComponent<NavMeshAgent>(); //Register navmesh agent
         rb = GetComponent<ActorPhysics>(); //Register actorphysics rb
         jumperContext.rb = rb; //Register that the jumper here is in fact this object's actorphysics script
-        if (ctr == null)
+        //if (ctr == null)
         {
             if (TryGetComponent(out EnemyController controller)) //Reads and registers the controller, followed by saving the animation IDs
             {
-                ctr = controller;
-                ctr.AddAnimationNames(animationCodeNames);
+                //ctr = controller;
+                //ctr.AddAnimationNames(animationCodeNames);
             }
         }
     }
 
-    public override void OnEnter()
-    {
-        agent.enabled = false;
-        timer = 0;
+    //public override void OnEnter()
+    //{
+        //agent.enabled = false;
+        //timer = 0;
         //ctr.PlayAnimation("JumpAttack");
-        jumperContext.currentJumpData = bigBlasterData;
-    }
+        //jumperContext.currentJumpData = bigBlasterData;
+    //}
 
     private /*public override*/ void WouldaBeenOnEnter() //Damn what's the override do? Anyway this piece runs each time you initiate this script. The script seems to always stay active, so start wouldn't work for this!
     {
         timer = 0; //Reset timers, just like you would on Start
         //ctr.PlayAnimation("JumpAttack"); //Plays an animation from its animator by name
         agent.enabled = false; //Deactivate pathfinding! Important!!! Otherwise your little buddy will go careening off during their attack
-        jumperContext.currentJumpData = bigBlasterData; //Set the Jumper logic's data to that of your enemy. This explains a bit of confusion. "DropKickerJumpData" is a script found elsewhere, so I need to make my own later
+        //jumperContext.currentJumpData = bigBlasterData; //Set the Jumper logic's data to that of your enemy. This explains a bit of confusion. "DropKickerJumpData" is a script found elsewhere, so I need to make my own later
         //EnemyJump.PerformJump(jumperContext); //Appears to tell a separate script to perform the actual jump portion of the attack. It's based on the old player jump. Contains no mentions of attacking
         //I guess this means that the Dropkicker is coded to always jump instantly upon the activation of its attack script, with the actual attack logic coming later
         rb.velocity += jumperContext.currentVelocity; //Seems to take the current speed and add... the current speed? Ah this just contains a Y value! Nevermind. Currently irrelevant for me. I'll skip other jump stuff
 
-        Vector3 dist = ctr.ctx.player.transform.position - rb.transform.position; //Make a vector and set it to the exact distance on all axes between the player and the enemy
-        dist.y = 0; //Delete the Y value... I always do this in an unwieldly extra calculation inside the initial vector creation whenever I do this... Why didn't I do it like this? It's so clean
-        rb.velocity += /*dropkickSpeed **/ dist.normalized; //Now set the enemy's speed to the desired speed, and multiply it by the normalised distance to give it a direction (toward the player)
+        //Vector3 dist = ctr.ctx.player.transform.position - rb.transform.position; //Make a vector and set it to the exact distance on all axes between the player and the enemy
+        //dist.y = 0; //Delete the Y value... I always do this in an unwieldly extra calculation inside the initial vector creation whenever I do this... Why didn't I do it like this? It's so clean
+        //rb.velocity += /*dropkickSpeed **/ dist.normalized; //Now set the enemy's speed to the desired speed, and multiply it by the normalised distance to give it a direction (toward the player)
 
-        Vector3 lookPos = ctr.ctx.player.transform.position; //Set up a Vector 3 and make it represent the player's location
-        lookPos.y = agent.transform.position.y; //Reset the Y value of lookPos to just be the enemy's height, preventing them from looking up or down
-        rotationTf.LookAt(lookPos); //Now just make a child containing all the visuals rotate to lookPos (real useful for Big Blaster)
+        //Vector3 lookPos = ctr.ctx.player.transform.position; //Set up a Vector 3 and make it represent the player's location
+        //lookPos.y = agent.transform.position.y; //Reset the Y value of lookPos to just be the enemy's height, preventing them from looking up or down
+        //rotationTf.LookAt(lookPos); //Now just make a child containing all the visuals rotate to lookPos (real useful for Big Blaster)
     }
 
     void OnUpdate()
@@ -79,10 +79,10 @@ public class BigBlasterAttack : EnemyAttack
         timer += Time.deltaTime; //Timer starts counting immediately! Remember that this stuff only runs during the actual attack, so no waiting for a funciton to be called. This is the function
         //Wait huh? Just deltaTime? It's giving an error here so is there some kinda reference to Time elsewhere in the script to make it part of this script's pool of variables?
         //Now follows mostly animation logic. This will be an issue for later! My only concern right now is getting each function in place
-        ctr.ExitAttack(this); //Stops this script from being executed, ending the attack sequence. Seems to be a custom function from somewhere. Gotta make sure this script is reset properly in OnUpdate
+        //ctr.ExitAttack(this); //Stops this script from being executed, ending the attack sequence. Seems to be a custom function from somewhere. Gotta make sure this script is reset properly in OnUpdate
         //More jump stuff. Basically just continues (by repeating) the functions called in the OnEnter function
         
-        if (timer > hitboxDelay) //Now this one's useful. I can just copy this straight up (as it seems to be from the Grab to the Dropkick). Times hitbox activation and deactivation. Neat!
+        //if (timer > hitboxDelay) //Now this one's useful. I can just copy this straight up (as it seems to be from the Grab to the Dropkick). Times hitbox activation and deactivation. Neat!
         { //I'm commenting all of it out for now since I don't want this giving errors in my cheat sheet
             /*if (timer < hitboxDelay + hitboxActiveTime)
             {
@@ -108,10 +108,10 @@ public class BigBlasterAttack : EnemyAttack
         //ctr.ctx.player.TakeKnockback(kb);
         //ctr.ctx.player.TakeDamage(grabDamage);
     }
-    public override void OnExit() //Seems like this is called when the script is stopped, no matter what. Its last hurrah. Or in other words: I don't have to worry about resetting it. Just do it here.
-    {
-        timer = 0;
-        agent.enabled = true;
+    //public override void OnExit() //Seems like this is called when the script is stopped, no matter what. Its last hurrah. Or in other words: I don't have to worry about resetting it. Just do it here.
+    //{
+        //timer = 0;
+        //agent.enabled = true;
         //recoveryTimer = 0;
         //playedRecovery = false;
         //rb.velocity = Vector3.zero;
@@ -119,13 +119,13 @@ public class BigBlasterAttack : EnemyAttack
         //grabHitbox.gameObject.SetActive(false);
     }
 
-    protected override void Reset() //Now what the fuck is this? Seems to add animations? No clue what protected and override mean. Seems to set "base" (EnemyAttack) back to its default values
-    {
+    //protected override void Reset() //Now what the fuck is this? Seems to add animations? No clue what protected and override mean. Seems to set "base" (EnemyAttack) back to its default values
+    //{
         //animationCodeNames.Add("JumpAttack");
         //animationCodeNames.Add("JumpAttackRecoverTrigger");
         //base.Reset();
-    }
-}
+    //}
+//}
 //Uses Jumper system to follow you
 //Faster than player
 //Chooses a random spot around you to pathfind towards

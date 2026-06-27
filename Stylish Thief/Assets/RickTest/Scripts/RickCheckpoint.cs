e485cca233f38c0f7675a250e4efd7ec5d8ac0a4
Rick-van-Leeuwen <rickvleeuwen10@gmail.com>
2026-06-27 18:10:41 +0200

    Break time

--- Stylish Thief/Assets/RickTest/Scripts/RickCheckpoint.cs
+++ Stylish Thief/Assets/RickTest/Scripts/RickCheckpoint.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RickCheckpoint : MonoBehaviour
 {
     RickDeathManager manager;
+    PlayerStateDriver playerStateDriver;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,14 +11,20 @@ public class RickCheckpoint : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /*void Update()
+    {if (playerStateDriver != null)
+        {
+            if (playerStateDriver.transform.parent.transform.localPosition.y < -5)
+            {
+                playerStateDriver.transform.parent.transform.localPosition = GetComponentInParent<RickDeathManager>().currentCheckpoint.transform.position;
+            }
+        }
+    }*/
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 6)
         {
+            playerStateDriver = other.gameObject.GetComponentInParent<PlayerStateDriver>();
             manager.currentCheckpoint = gameObject;
         }
     }

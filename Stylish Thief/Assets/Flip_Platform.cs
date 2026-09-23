07851acbc025b8ae86b45e6b1c0b81c824abd536
Max <max.van.der.meer12@gmail.com>
2026-09-23 16:02:26 +0200

    Worked on the flip platform, it now flips, but not yet with pauses

--- Stylish Thief/Assets/Flip_Platform.cs
+++ Stylish Thief/Assets/Flip_Platform.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using System.Collections;
 
 public class Flip_Platform : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject FlipPlat;
+    public float FlipSpeed = 200f;
+    public Vector3 rotate;
+
     void Start()
     {
-        
+        StartCoroutine(Flipping());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Flipping()
     {
-        
+        while (rotate != new Vector3(FlipSpeed * Time.deltaTime, 0f, 0f))
+        {
+            transform.Rotate(new Vector3(FlipSpeed * Time.deltaTime, 0f, 0f));
+        }
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(Flipping());
     }
 }
